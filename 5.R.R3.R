0c7504c03453b5d736090ac16e53bcfc11eb8162
jiahui604 <jiahui@MacBook-Pro.local>
2017-12-12 23:15:26 -0600

    added block bootstraping

--- 5.R.R3.R
+++ 5.R.R3.R
@@ -29,14 +29,21 @@ se.fn(Xy,1:1000)
 set.seed(17)
 se.fn(Xy,sample(1:1000,1000,replace = TRUE))
 
+require(boot)
 boot.out= boot(Xy,se.fn, R=1000)
 boot.out
 
+#5.R.R4 Block bootstrapping
+#Finally, use the block bootstrap to estimate s.e.(β^1). 
+#Use blocks of 100 contiguous observations, and resample ten whole blocks with replacement 
+#then paste them together to construct each bootstrap time series.
+tsboot(Xy, se.fn, R = 1000, sim = "fixed", l = 100)
 
-#bootstrap - standard error 2
+#######bootstrap - standard error 2, another way to calculate s.e. for bootstrapping
 boot.fn <- function(data, index) {
   lr <- lm(y~.,data=Xy,subset = index)
   return (coef(lr))
 }
 
 boot(Xy,boot.fn,R=1000)
+
